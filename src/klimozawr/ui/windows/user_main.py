bed58b5bc8d32ba716b0232b14987d7aed11a25e
denkerwis <klimizaak75@gmail.com>
2025-12-29 22:21:22 +0300

    ui: increase tile text size for regular user

--- src/klimozawr/ui/windows/user_main.py
+++ src/klimozawr/ui/windows/user_main.py
@@ -1,10 +1,9 @@
 from __future__ import annotations
 
-from PySide6.QtWidgets import QMenuBar, QMenu, QSplitter, QWidget, QVBoxLayout
+from PySide6.QtWidgets import QMenuBar, QMenu, QWidget, QVBoxLayout
 from PySide6.QtGui import QAction
 
 from klimozawr.ui.widgets.device_cards import DeviceCardsView
-from klimozawr.ui.widgets.device_details import DeviceDetailsPanel
 from klimozawr.ui.windows.base_main import BaseMainWindow
 from klimozawr.ui.strings import tr
 
@@ -19,26 +18,17 @@ class UserMainWindow(BaseMainWindow):
         menu_file = QMenu(tr("menu.title"), self)
         self.action_logout = QAction(tr("menu.logout"), self)
         self.action_exit = QAction(tr("menu.exit"), self)
-        self.action_export_logs = QAction(tr("menu.export_logs_all"), self)
         menu_file.addAction(self.action_logout)
-        menu_file.addAction(self.action_export_logs)
         menu_file.addAction(self.action_exit)
         menubar.addMenu(menu_file)
         self.setMenuBar(menubar)
 
         # ВАЖНО: fit_viewport=True → плитки растягиваются на весь экран
-        self.cards = DeviceCardsView(fit_viewport=True, text_scale=1.15)
-        self.details = DeviceDetailsPanel()
-
-        monitor_split = QSplitter()
-        monitor_split.addWidget(self.cards)
-        monitor_split.addWidget(self.details)
-        monitor_split.setStretchFactor(0, 3)
-        monitor_split.setStretchFactor(1, 2)
+        self.cards = DeviceCardsView(fit_viewport=True, text_scale=1.4)
 
         monitor_tab = QWidget()
         ml = QVBoxLayout()
-        ml.addWidget(monitor_split)
+        ml.addWidget(self.cards)
         monitor_tab.setLayout(ml)
 
         self.setCentralWidget(monitor_tab)
